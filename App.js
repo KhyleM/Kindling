0fadb71bd828de8af8d0f7531e9d7814cb980c17
Khyle Mott <khylemott@gmail.com>
2019-01-01 19:23:40 -0500

    added cascading cards, changed images

--- App.js
+++ App.js
@@ -10,13 +10,13 @@ import Deck from './src/Deck';
 
 
 const DATA = [
-  { id: 1, text: 'Card #1', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
-  { id: 2, text: 'Card #2', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
-  { id: 3, text: 'Card #3', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 1, text: 'Card #1', uri: 'https://i.ytimg.com/vi/hbgsv9CJzl4/maxresdefault.jpg' },
+  { id: 2, text: 'Card #2', uri: 'https://consequenceofsound.files.wordpress.com/2018/08/nicki-minaj-harriet-tubman.png?w=807' },
+  { id: 3, text: 'Card #3', uri: 'https://ichef.bbci.co.uk/images/ic/960x540/p01br7g4.jpg' },
   { id: 4, text: 'Card #4', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
-  { id: 5, text: 'Card #5', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-04.jpg' },
-  { id: 6, text: 'Card #6', uri: 'http://www.fluxdigital.co/wp-content/uploads/2015/04/Unsplash.jpg' },
-  { id: 7, text: 'Card #7', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-09.jpg' },
+  { id: 5, text: 'Card #5', uri: 'https://i.ytimg.com/vi/hbgsv9CJzl4/maxresdefault.jpg' },
+  { id: 6, text: 'Card #6', uri: 'https://consequenceofsound.files.wordpress.com/2018/08/nicki-minaj-harriet-tubman.png?w=807' },
+  { id: 7, text: 'Card #7', uri: 'https://ichef.bbci.co.uk/images/ic/960x540/p01br7g4.jpg' },
   { id: 8, text: 'Card #8', uri: 'http://imgs.abduzeedo.com/files/paul0v2/unsplash/unsplash-01.jpg' },
 ];
 
@@ -42,12 +42,27 @@ export default class App extends React.Component {
     );
   }
 
+  renderNoMoreCards() {
+    return (
+      <Card title="Out of cards!">
+        <Text style={{ marginBottom: 10 }}>
+          There's no more content here!
+        </Text>
+        <Button
+        backgroundColor="#03A9F4"
+        title="Get more!"
+        />
+      </Card>
+    )
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Deck 
           data={DATA}
           renderCard={this.renderCard}
+          renderNoMoreCards={this.renderNoMoreCards}
         />
       </View>
     );
--- src/Deck.js
+++ src/Deck.js
@@ -15,6 +15,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+    static defaultProps = {
+        onSwipeRight: () => {},
+        onSwipeLeft: () => {}
+    }
+
     constructor(props) {
         super(props);
 
@@ -36,7 +41,7 @@ class Deck extends Component {
         });
 
 
-        this.state = { panResponder, position };
+        this.state = { panResponder, position, index: 0 };
     }
 
     forceSwipe(direction) {
@@ -44,7 +49,16 @@ class Deck extends Component {
         Animated.timing(this.state.position, {
             toValue: { x, y: 0 },
             duration: SWIPE_OUT_DURATION
-        }).start();
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction) {
+        const { onSwipeLeft, onSwipeRight, data } = this.props;
+        const item = data[this.state.index];
+
+        direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+        this.state.position.setValue({ x: 0, y: 0});
+        this.setState({ index: this.state.index + 1 });
     }
 
     resetPosition() {
@@ -75,20 +89,35 @@ class Deck extends Component {
     }
 
     renderCards() {
-        return this.props.data.map((item, index) => {
-            if (index === 0) {
+        if (this.state.index >= this.props.data.length) {
+            return this.props.renderNoMoreCards();
+        }
+
+
+        return this.props.data.map((item, i) => {
+            if (i < this.state.index) { return null; }
+
+            if (i === this.state.index) {
                 return (
                     <Animated.View
                      key={item.id}
-                     style={this.getCardStyle()}
+                     style={[this.getCardStyle(), styles.cardStyle]}
                      {...this.state.panResponder.panHandlers}
                     >
                         {this.props.renderCard(item)}
                     </Animated.View>
-                )
+                );
             }
-            return this.props.renderCard(item);
-        });
+
+            return (
+                <Animated.View 
+                    key={item.id} 
+                    style={[styles.cardStyle, { top: 10 * (i - this.state.index) }]}
+                >
+                 {this.props.renderCard(item)}
+                </Animated.View>
+            );
+        }).reverse();
     }
     render() {      
         return (
@@ -98,5 +127,13 @@ class Deck extends Component {
         )
     }
 }
+
+const styles = {
+    cardStyle: {
+        position: 'absolute',
+        width: SCREEN_WIDTH,
+        top: 100
+    }
+};
 
 export default Deck;
